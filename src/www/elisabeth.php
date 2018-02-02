32e628f7aafc378b27281ddf66cfaa410ed8c824
ElisabethSH <e.haabegaard@gmail.com>
2018-02-02 14:59:44 +0100

    Details

--- src/www/elisabeth.php
+++ src/www/elisabeth.php
@@ -6,8 +6,15 @@
 </head>
 <body>
 
-<h1>Hello Elisabeth</h1>
-<p> I am 26 years old </p>
+<h1>Elisabeth</h1>
+<h2>26 years old and live in Nørrebro </h2
+
+<ul> 
+
+<li>Geologist</li>
+<li> Soon to be programmer </li>
+<li>26 years old </>
+<li>Nørrebro </li>
 
 
 </body>
