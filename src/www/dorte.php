ee7bacc3341c8ffcc9c01b8336ea989a44596611
Martin Kæstel Nielsen <martin@think.dk>
2018-02-02 10:31:42 +0100

    initial commit

---
+++ src/www/dorte.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page title</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+<h1>Hello Dorte</h1>
+
+</body>
+</html>
---
+++ src/www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page title</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+<h1>Hello World</h1>
+
+<a href="dorte.php">Dorte</a><br />
+<a href="bolette.php">Bolette</a><br />
+<a href="mads.php">Mads</a><br />
+<a href="elisabeth.php">Elisabeth</a><br />
+<a href="sarah.php">Sarah</a><br />
+<a href="peter.php">Peter</a><br />
+<a href="octavian.php">Octavian</a>
+
+</body>
+</html>
