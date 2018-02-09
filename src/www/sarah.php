46a295f3f9bf375f6e02ce5928f9eaeb5ca52739
Sarah E <>
2018-02-09 15:15:53 +0100

    Adding styles to KBHFF text

--- src/www/sarah.php
+++ src/www/sarah.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Page title</title>
+	<title>Sarahs info om KBHFF støttepose</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style media="screen">
+/* Unsure what the media="screen" means. Added automatically by Atom */
+		div.intro {background-color: cyan}
+		div.kbhff_info {margin-left: 25px; background-color: LightGray}
+		div.mere_info {background-color: Lightgreen}
+		.stotte {color: red; font-weight: bold;}
+/* Note doesn't take p. or class. ahead of class name. Why? */
+		a {text-decoration: none;}
+/* Removes underline from links */
+		body {font-family: verdana; margin-left: 25px; margin-right: 100px}
+	</style>
 </head>
 <body>
-	<h1>Den ny støttepose [[info og annoncering]]</h1>
+	<h1>Den ny <span class="stotte">støttepose</span> [[info og annoncering]]</h1>
 	<div class="intro">
-		<h2>Vil du være støttemedlem?</h2>
-		<strong>Du kan nu købe den nye støttepose. Kun for støttemedlemmer!</strong>
-		<h3>Hvad vil det sige at være et støttemedlem?</h3>
+		<h2>Vil du være <span class="stotte">støttemedlem</span>?</h2>
+		<strong>Du kan nu købe den nye <span class="stotte">støttepose</span>. Kun for støttemedlemmer!</strong>
+		<h3>Hvad vil det sige at være et <span class="stotte">støttemedlem</span>?</h3>
 		<!-- add class for støttemedlem/støttepose/osv -->
-		<p>At være støttemedlem i KBHFF betyder at du støtter op om KBHFF ved at købe poser til en lidt højere pris end andre medlemmer. Til gengæld forventes du så ikke at tage vagter og lægge frivilligt arbejde i foreningen som andre medlemmer gør.</p>
+		<p>At være <span class="stotte">støttemedlem</span> i KBHFF betyder at du støtter op om KBHFF ved at købe poser til en lidt højere pris end andre medlemmer. Til gengæld forventes du så ikke at tage vagter og lægge frivilligt arbejde i foreningen som andre medlemmer gør.</p>
 	</div>
 	<div class="kbhff_info">
 		<h2>Hvad er KBHFF?</h2>
