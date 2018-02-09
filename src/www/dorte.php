c7071b6abffddd7589079d084f8c70884810f1da
DorteMP <36069790+DorteMP@users.noreply.github.com>
2018-02-09 14:31:48 +0100

    HTML testing

--- src/www/dorte.php
+++ src/www/dorte.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Page title</title>
+	<title>Den nye pose - info og annoncering</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
 
-<h1>Hello Dorte</h1>
-<h2>Hi</h2>
+<h1>Den nye pose - info og annoncering</h1>
+<h2>Info til støttemedlemmer</h2>
+<p></p>
+<h2>Ny pose 125. kr for støttemedlemmer</h2>
+<p></p>
+<h2>Hvad vil det sige at være et støttemedlem</h2>
+<p></p>
+<h2>Praktisk med afhentning</h2>
+<h3>I hvilke afdelinger kan man få det</h3>
+<p></p>
+<h3>Tidsrum</h3>
+<p></p>
+<h3>Forudbetaling</h3>
+<h2>Vores handlingsplan</h2>
+<p></p>
+<h2>Hvordan bliver man medlem</h2>
+<p></p>
+<h3>Introduktion til Kbhff før indmeldelse</h3>
+<p></p>
+<h2>Om foreningen</h2>
+<h3/>En medlemsdrevet forening</h3>
+<img src="images_dorte/ny-struktur.003.jpg"</img src>
+<p>Københavns Fødevarefællesskab er en medlemsejet og -drevet indkøbsforening, der fokuserer på at tilbyde økologiske, velsmagende, lokalt producerede og bæredygtige fødevarer i et sæsonbaseret udbud til priser, hvor alle kan være med.</p>
+<h3>Grundprincipperne</h3>
+<p>For os er “forbrugerindflydelse” ikke blot muligheden for at kunne vælge mellem forskellige produkter. I fødevarefællesskabet er kunderne både medlemmer, medarbejdere og medejere.</p>
+<h3>Vision/forretningsplan</h3>
+<p></p>
+<h3>Refusion</h3>
+<p></p>
+<h3>Disclaimer for fejl og mangler</h3>
+<p></p>
+<h3>svagheder vedrørende medlemsdrevet arbejde</h3>
+<p></p>
+<h3>Annonceringsstrategi</h3>
+<p>Undervejs...</p>
+
 </body>
 </html>
