836bc921d783db99b9f63f45b5e96078a22aaad9
ElisabethSH <e.haabegaard@gmail.com>
2018-02-09 13:18:08 +0100

    food blog update

--- src/www/elisabeth.php
+++ src/www/elisabeth.php
@@ -1,43 +1,143 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Elisabeth</title>
+	<title>Elisabeth's madblog</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<style>
-	h1 {
-    background-color: red;
-    color:black;
-    font-family:verdana;
-    font-size: 30px;
-    margin-top: 30px;
-    margin-left: 80px;
-    margin-right: 1280px}
-
-		li {
-			color:brown;
-			font-family: times;
-			font-size: 20 px;
-			margin: 5px 15px 20px 80px;
+	body{
+		font-family: sans-serif;
+		margin-left: 250px;
+	}
+		h1 {
+			font-size: 30px;
+			font-family: inherit;
+			font-weight: bold;
+			margin-left: 15%;
+		}
+		h2.beskrivelse {
+			font-size: 14px;
+			color: grey;
+			font-style: italic;
+			margin-left: 22%;
+		}
+		h2.kategori {
+			font-size: 14px;
+			color: grey;
+			font-style: italic;
+			margin-top: 2px;
+		}
+		h2.overskrift {
+			font-size: 28px;
+			font-family: inherit;
+			font-weight: lighter;
+			margin-bottom: 2px;
+		}
+		div.historien {
+			margin-right: 650px;
+			text-align: left;
+		}
+		h2.overskrift2 {
+			font-size: 30px;
+			font-family: inherit;
+			font-weight: bold;
+		}
+		ul.about {
+			font-size: 14px;
+			font-style: italic;
+			color: grey;
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+		}
+		h3 {
+			margin: 40px 0 10px 0;
+		}
+		h4 {
+			margin: 0;
+		}
+		ul.crust {
+			list-style-type: none;
+			margin: 0 6px 15px 0;
+			padding: 0 6px 15px 0;
+			font-weight: lighter;
+}
+		ul.sauce {
+			list-style-type: none;
+			margin: 0 6px 15px 0;
+			padding: 0 6px 15px 0;
+			font-weight: lighter;
+		}
+		ul.topping {
+			list-style-type: none;
+			margin: 0 6px 15px 0;
+			padding: 0 6px 15px 0;
+			font-weight: lighter;
+		}
+		ol.procedure {
+		margin: 0 6px 15px 0;
+		padding: 0 6px 15px 0;
+		font-weight: lighter;
 		}
 	</style>
 </head>
 <body>
-
-
-
-<h1>Elisabeth</h1>
-<h2>26 years old and live in Nørrebro </h2>
-
-
-<ul>
-<li>Geologist</li>
-<li>Soon to be a programmer</li>
-<li>26 years old</li>
-<li>Nørrebro</li>
+<h1>Elisabeth's yummie and hearty recipes</h1>
+<h2 class="beskrivelse">Healthy and cruelty free recipes made from the heart</h2>
+<h2 class="overskrift">Kale burger with veggie paddie</h2>
+<h2 class="kategori">Categories: Dinner, Vegetarian, Low-carb, Burger</h2>
+<img src="images_elisabeth/27901430_207398990003336_1273595914_o.jpg" alt="Kale Burger" style="width: 600px; height: 500px">
+<div class="historien">
+	<p>This kale pizza is truly made from the heart. It is a low-carb, super healthy and all organic recepie which is high in fibers, vitamins and minerals. And as you can see it tastes good as well! One piece of pizza only contains 100 kcal, but still contains 6 grams of protein, 7 grams of carbs and 5 grams of fats! It is the perfect distribution of the macro nutriens if you are up for a healhy lifestyle.</p>
+	<p>I usually make one or two of these pizzas to impress my hungry guests and it never goes wrong. Everybody simply loves my kale-pizza! I am usually very full after three pieces, but if you have leftovers one or two pieces form a really nice lunch the day after. </p>
+	<br/>
+	___________________________________________________________________________
+</div>
+<h2 class="overskrift2">Kale pizza with veggies</h2>
+<ul class="about">
+	<li>10 slices of pizza</li>
+	<li>Preparation time: 45 minutes</li>
+	<li>Total time: 1 hour 15 minutes</li>
 </ul>
-
-<hej> du er en kat </hej>
-
+<h3>Ingredients</h3>
+<h4>The Crust</h4>
+<ul class="crust">
+	<li>300 grams of organic sweetheart cabbage</li>
+	<li>120 grams of organic kale</li>
+	<li>100 grams of organic white onion</li>
+	<li>3 cloves of organic garlic</li>
+	<li>2 organic, free range eggs</li>
+	<li>1 tablespoon of organic quinoa</li>
+	<li>1 tablespoon of organic tahini</li>
+	<li>1 tablespoon of corianderseeds</li>
+	<li>1 tablespoon of salt</li>
+	<li>Some lemon juice</li>
+</ul>
+<h4>The Sauce</h4>
+<ul class="sauce">
+	<li>400 grams of organic tomatoes</li>
+	<li>Mixed spices of your own preference</li>
+	<li>1 Oranic white onion</li>
+</ul>
+<h4>The Topping</h4>
+<ul class="topping">
+	<li>125 grams of organic mozarella cheese</li>
+	<li>2 organic carrots</li>
+	<li>75 grams of organic bell pepper</li>
+	<li>3 organic portobello mushrooms</li>
+</ul>
+<h3>Procedure</h3>
+<ol class="procedure">
+	<li>Chop the cabbage, kale, onion and garlic in a mini chopper. Add in the quinoa, tahini, spices and lemon juice. Stir well. Mix the mass with the eggs.</li>
+	<li>Spread the mass in a thin layer on a baking pan.</li>
+	<li>Bake the crust for 20 minutes.</li>
+	<li>Mix the ingredients for the sauce in a hot pan.</li>
+	<li>When the crust is hard and attains a golden glow, take it out and spread out he sauce.</li>
+	<li>Cut the cheese and veggies in thin slices and add it all to the pizza.</li>
+	<li>Bake the pizza for 15 minutes.</li>
+	<li>When the cheese is melting and the pizza looks yummie take it out and dinner is ready to be served!</li>
+</ol>
+<img src="images_elisabeth/27849308_207398993336669_1740407598_n.jpg" alt="Kale Burger in progress"
+<p>Af: Elisabeth Haabegaard</p>
 </body>
 </html>
 #
