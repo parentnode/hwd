8c163c52e3813b9db79912d5687d66dee9416ce5
Sarah E <>
2018-02-09 14:34:12 +0100

    HTML test with KBHFF text

--- src/www/sarah.php
+++ src/www/sarah.php
@@ -1,30 +1,53 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Page title</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	</head>
-	<body>
+<head>
+	<title>Page title</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+	<h1>Den ny støttepose [[info og annoncering]]</h1>
+	<div class="intro">
+		<h2>Vil du være støttemedlem?</h2>
+		<strong>Du kan nu købe den nye støttepose. Kun for støttemedlemmer!</strong>
+		<h3>Hvad vil det sige at være et støttemedlem?</h3>
+		<!-- add class for støttemedlem/støttepose/osv -->
+		<p>At være støttemedlem i KBHFF betyder at du støtter op om KBHFF ved at købe poser til en lidt højere pris end andre medlemmer. Til gengæld forventes du så ikke at tage vagter og lægge frivilligt arbejde i foreningen som andre medlemmer gør.</p>
+	</div>
+	<div class="kbhff_info">
+		<h2>Hvad er KBHFF?</h2>
+		<h3>Introduktion til KBHFF</h3>
+		<ol>
+			<li>Grundprincipperne</li>
+			<li>Vision/forretningsplan</li>
+		</ol>
+		<p>KBHFF er en medlemsdrevet forening. Medlemmer i KBHFF tager vagter, hvor de indkøber, pakker og udleverer grøntsager til andre medlemmer. Fælleskabet skabes igennem fællesspisning, møder med vores avlere, medlemsmøder og workshops.</p>
+		<h3>Hvordan bliver man medlem?</h3>
+		<ol>
+			<li>Hjemmesiden vs afdelingen</li>
+			<li>Hvilke afdelinger kan man købe den nye pose i.</li>
+			<ol>
+				<li>Hvornår har de åbent?</li>
+				<li>Hvordan betaler man / køber man en pose?</li>
+			</ol>
+	</div>
+	<div class="mere_info">
+		<h2>Yderligere info</h2>
+		<p>Hvis du vil lære mere om KBHFF, de grøntsager du kan købe og lokalafdelingerne, kan du læse videre her:</p>
+		<ul>
+			<li><a href="http://kbhff.dk/afdelinger/">Lokalafdelinger</a></li>
+			<li><a href="http://kbhff.dk/grontsager/">Grøntsagerne</a></li>
+			<li><a href="http://kbhff.dk/om-kbhff/">Om KBHFF</a></li>
+		</ul>
+	</div>
+	<hr>
+	<p>Test</p>
+	<p>More testing</p>
+	<p>
+		Line break
+		<br />
+		Line break
+	</p>
 
-		<h1>Hello team</h1>
-		<h2>Hello Sarah</h2>
-		<h3>h3</h3>
-		<h4>h4</h4>
-		<h5>h5</h5>
-		<i>Italics</i>
-		<br />
-		<b>Bold</b>
-		<br />
-		<br />
-		<br />
-		Test
-		<br />
-		More testing
-		<br />
-		Atom test
-		<br />
-		Testing Atom committing 2
-		<br />
 	</body>
 </html>
 
