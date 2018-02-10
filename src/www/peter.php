772c5101fd0d65ee76405d7c77c032080a485291
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-10 03:35:52 +0100

    update my site

--- src/www/peter.php
+++ src/www/peter.php
@@ -1,28 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Peter's site</title>
+	<title>The best site of all time</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
+<style>
 
+</style>
 
-<h1>Hello!</h1>
+<h1>Hello there!</h1>
+<h2>Welcome to my site :)</h2>
 
+<div class="ordered-lists-test">
+	<ol>
+		<li>First</li>
+		<li>Second</li>
+		<li>Third</li>
+	</ol>
+</div>
 
+<div class="unordered-lists-test">
+	<ul>
+		<li>Count</li>
+		<li>the rest</li>
+		<li>yourself!</li>
+	</ul>
+</div>
 
+<div class="random text">
+	<p>A line of text</p>
+	<p>Another line of text</p>
+</div>
 
+<div class="lorem-ipsum text">
+	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum omnis numquam veniam rem atque eos nihil laboriosam ullam dolor repudiandae, quo quae. Voluptate sapiente laborum facere in non perferendis dignissimos!</p>
+</div>
 
-
-
-
-
-
-
-<h2><?php print "hello" ?></h2>
+<h3><?php print "php is working!" ?></h3>
 <?// phpinfo() ?>
 <?
-	$mysqli = new mysqli("127.0.0.1", "root", "#PASS#");
+	$mysqli = new mysqli("127.0.0.1", "root", "#PASSWORD#");
 	print_r($mysqli);
 ?>
 </body>
+</html>
