9b73a4683a70b3e0f652c66a3199f8ddd3f78fc5
Martin Kæstel Nielsen <martin@think.dk>
2018-02-12 10:38:46 +0100

    Re-added peter + octavian (password)

---
+++ src/www/octavian.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page title</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+<h1>Hello Octavian</h1>
+<?
+	$mysqli = new mysqli("127.0.0.1", "root", "#PASS#");
+	print_r($mysqli);
+?>
+
+<p>Hello</p>
+
+</body>
+#
+	Hej
+
+#
+
+#
+</html>
