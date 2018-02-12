3db4f706216952d6e1f15cd4233c4172854191b7
ElisabethSH <e.haabegaard@gmail.com>
2018-02-12 09:55:07 +0100

    Comment box

--- src/www/elisabeth.php
+++ src/www/elisabeth.php
@@ -7,197 +7,319 @@
 	body{
 		font-family: sans-serif;
 		margin-left: 250px;
+		margin-right: 250px;
+		box-sizing: border-box;
 	}
-		h1 {
-			font-size: 30px;
-			font-family: inherit;
-			font-weight: bold;
-			margin-left: 15%;
-		}
-		h2.beskrivelse {
-			font-size: 14px;
-			color: grey;
-			font-style: italic;
-			margin-left: 22%;
-		}
-		h2.kategori {
-			font-size: 14px;
-			color: grey;
-			font-style: italic;
-			margin-top: 2px;
-		}
-		h2.overskrift {
-			font-size: 28px;
-			font-family: inherit;
-			font-weight: lighter;
-			margin-bottom: 2px;
-		}
-		div.historien {
-			margin-right: 650px;
-			text-align: left;
-		}
-		h2.overskrift2 {
-			font-size: 30px;
-			font-family: inherit;
-			font-weight: bold;
-		}
-		ul.about {
-			font-size: 14px;
-			font-style: italic;
-			color: grey;
-			list-style-type: none;
-			margin: 0;
-			padding: 0;
-		}
-		h3 {
-			margin: 40px 0 10px 0;
-		}
-		h4 {
-			margin: 0;
-		}
-		ul.crust {
-			list-style-type: none;
-			margin: 0 6px 15px 0;
-			padding: 0 6px 15px 0;
-			font-weight: lighter;
-}
-		ul.sauce {
-			list-style-type: none;
-			margin: 0 6px 15px 0;
-			padding: 0 6px 15px 0;
-			font-weight: lighter;
-		}
-		ul.topping {
-			list-style-type: none;
-			margin: 0 6px 15px 0;
-			padding: 0 6px 15px 0;
-			font-weight: lighter;
-		}
-		ol.procedure {
+	h1 {
+		font-size: 30px;
+		font-family: inherit;
+		font-weight: bold;
+		margin-left: 25%;
+	}
+	h2.beskrivelse {
+		font-size: 14px;
+		color: grey;
+		font-style: italic;
+		margin-left: 30%;
+	}
+	h2.kategori {
+		font-size: 14px;
+		color: grey;
+		font-style: italic;
+		margin-top: 2px;
+	}
+	h2.overskrift {
+		font-size: 28px;
+		font-family: inherit;
+		font-weight: lighter;
+		margin-bottom: 2px;
+		margin-top: 20px;
+		padding-top: 40px;
+	}
+	div.historien {
+		margin-right: 175px;
+		text-align: left;
+	}
+	h2.overskrift2 {
+		font-size: 30px;
+		font-family: inherit;
+		font-weight: bold;
+	}
+	ul.about {
+		font-size: 14px;
+		font-style: italic;
+		color: grey;
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+	h3 {
+		margin: 40px 0 10px 0;
+	}
+	h4 {
+		margin: 0;
+	}
+	ul.ingredients {
+		list-style-type: none;
 		margin: 0 6px 15px 0;
 		padding: 0 6px 15px 0;
-		margin-right: 650px;
 		font-weight: lighter;
-		}
-		table, th, td {
-			border: 1px solid black;
-			background-color: whitesmoke;
-			width: 600px;
+	}
+	ol.procedure {
+		margin: 0 6px 15px 0;
+		padding: 0 6px 15px 0;
+		margin-right: 220px;
+		margin-left: 20px;
+		font-weight: lighter;
+	}
+	table, th, td {
+		border: 1px solid black;
+		background-color: whitesmoke;
+		width: 600px;
+	}
+	div.menu {
+		list-style-type: none;
+		margin: 0 0 15px 0;
+		padding: 0 0 15px 0 ;
+		overflow: hidden;
+		float: left;
+	}
+	li.submenu {
+		float: left;
+		list-style-type: none;
+		margin-right: 10px;
+		margin-left: 100px;
+		margin-bottom: 10 px;
+	}
 
-		}
+	hr.menuhr {
+		margin-top: 50px;
+	}
+	h2.welcome {
+		font-size: 18px;
+		font-weight: lighter;
+		text-align: center;
+	}
+	.me {
+		letter-spacing: 1px;
+		font-size: 12px;
+	}
+	.main {
+		width: 75%;
+		float:left;
+	}
+	.col2 {
+		width: 25%;
+		float: left;
+		margin-top: 5%;
+	}
+	.dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: #f9f9f9;
+		min-width: 160px;
+		box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		z-index: 1;
+	}
+	.dropdown:hover .dropdown-content {
+		display: block;
+}
+	.desc {
+		padding: 15px;
+		text-align: center;
+	}
+	li a, .dropbtn {
+		display: inline-block;
+		text-align: center;
+	}
+	li a:hover, .dropdown:hover .dropbtn {
+		background-color: #31B404;
+	}
+	li.dropdown {
+		display: inline-block;
+	}
+.dropdown-content a {
+	padding: 12px 16px;
+	display: block;
+	text-align: left;}
+	}
+	.dropdown-content a:hover {background-color: #f1f1f1}
+	.dropdown:hover .dropdown-content {
+		display: block;
+	}
+	.formularspr {
+		font-size: 20px;
+		margin-top: 80px;
+	}
 	</style>
 </head>
 <body>
-<h1>Elisabeth's yummie and hearty recipes</h1>
-<h2 class="beskrivelse">Healthy and cruelty free recipes made from the heart</h2>
-<h2 class="overskrift">Kale burger with veggie paddie</h2>
-<h2 class="kategori">Categories: Dinner, Vegetarian, Low-carb, Burger</h2>
-<img src="images_elisabeth/27901430_207398990003336_1273595914_o.jpg" alt="Kale Burger" style="width: 600px; height: 500px">
-<div class="historien">
-	<p>This kale pizza is truly made from the heart. It is a low-carb, super healthy and all organic recepie which is high in fibers, vitamins and minerals. And as you can see it tastes good as well! One piece of pizza only contains 100 kcal, but still contains 6 grams of protein, 7 grams of carbs and 5 grams of fats! It is the perfect distribution of the macro nutriens if you are up for a healhy lifestyle.</p>
-	<p>I usually make one or two of these pizzas to impress my hungry guests and it never goes wrong. Everybody simply loves my kale-pizza! I am usually very full after three pieces, but if you have leftovers one or two pieces form a really nice lunch the day after. </p>
-	<br/>
-	___________________________________________________________________________
+<div class="header">
+	<h1>Elisabeth's yummie and hearty recipes</h1>
+	<h2 class="beskrivelse">Healthy and cruelty free recipes made from the heart</h2>
+	<hr>
+		<ul class="menu">
+			<li class="submenu">Home</li>
+			<li class="submenu">Recipes</li>
+			<li class="submenu dropdown">
+	    <a href="javascript:void(0)" class="dropbtn">Sustainability</a>
+	    <div class="dropdown-content">
+	      <a href="http://www.kbhff.dk">KBHFF</a>
+	      <a href="http://www.think.dk">Think</a>
+	      <a href="http://www.suztain.dk">Suztain</a>
+	    </div>
+		</li>
+			<li class="submenu"><a href="images_elisabeth\about.html">About</a></li>
+			<li class="submenu">Contact</li>
+		</ul>
+	<hr class="menuhr">
 </div>
-<h2 class="overskrift2">Kale pizza with veggies</h2>
-<ul class="about">
-	<li>10 slices of pizza</li>
-	<li>Preparation time: 45 minutes</li>
-	<li>Total time: 1 hour 15 minutes</li>
-</ul>
-<h3>Ingredients</h3>
-<h4>The Crust</h4>
-<ul class="crust">
-	<li>300 grams of organic sweetheart cabbage</li>
-	<li>120 grams of organic kale</li>
-	<li>100 grams of organic white onion</li>
-	<li>3 cloves of organic garlic</li>
-	<li>2 organic, free range eggs</li>
-	<li>1 tablespoon of organic quinoa</li>
-	<li>1 tablespoon of organic tahini</li>
-	<li>1 tablespoon of corianderseeds</li>
-	<li>1 tablespoon of salt</li>
-	<li>Some lemon juice</li>
-</ul>
-<h4>The Sauce</h4>
-<ul class="sauce">
-	<li>400 grams of organic tomatoes</li>
-	<li>Mixed spices of your own preference</li>
-	<li>1 Oranic white onion</li>
-</ul>
-<h4>The Topping</h4>
-<ul class="topping">
-	<li>125 grams of organic mozarella cheese</li>
-	<li>2 organic carrots</li>
-	<li>75 grams of organic bell pepper</li>
-	<li>3 organic portobello mushrooms</li>
-</ul>
-<h3>Procedure</h3>
-<ol class="procedure">
-	<li>Chop the cabbage, kale, onion and garlic in a mini chopper. Add in the quinoa, tahini, spices and lemon juice. Stir well. Mix the mass with the eggs.</li>
-	<li>Spread the mass in a thin layer on a baking pan.</li>
-	<li>Bake the crust for 20 minutes.</li>
-	<li>Mix the ingredients for the sauce in a hot pan.</li>
-	<li>When the crust is hard and attains a golden glow, take it out and spread out he sauce.</li>
-	<li>Cut the cheese and veggies in thin slices and add it all to the pizza.</li>
-	<li>Bake the pizza for 15 minutes.</li>
-	<li>When the cheese is melting and the pizza looks yummie take it out and dinner is ready to be served!</li>
-</ol>
-<table>
-	<caption>Nutritional values</caption>
+<div class="main">
+	<h2 class="overskrift">Kale burger with veggie paddie</h2>
+	<h2 class="kategori">Categories: Dinner, Vegetarian, Low-carb, Burger</h2>
+	<img src="images_elisabeth/27846464_207399026669999_591757260_o.jpg" alt="Kale Burger" style="width: 600px; height: 400px">
+	<div class="historien">
+		<p>This kale pizza is truly made from the heart. It is a low-carb, super healthy and all organic recepie which is high in fibers, vitamins and minerals. And as you can see it tastes good as well! One piece of pizza only contains 100 kcal, but still contains 6 grams of protein, 7 grams of carbs and 5 grams of fats! It is the perfect distribution of the macro nutriens if you are up for a healhy lifestyle.</p>
+		<p>I usually make one or two of these pizzas to impress my hungry guests and it never goes wrong. Everybody simply loves my kale-pizza! I am usually very full after three pieces, but if you have leftovers one or two pieces form a really nice lunch the day after. </p>
+		<hr>
+	</div>
+	<h2 class="overskrift2">Kale pizza with veggies</h2>
+	<ul class="about">
+		<li>10 slices of pizza</li>
+		<li>Preparation time: 45 minutes</li>
+		<li>Total time: 1 hour 15 minutes</li>
+	</ul>
+	<h3>Ingredients</h3>
+	<h4>The Crust</h4>
+	<ul class="ingredients">
+		<li>300 grams of organic sweetheart cabbage</li>
+		<li>120 grams of organic kale</li>
+		<li>100 grams of organic white onion</li>
+		<li>3 cloves of organic garlic</li>
+		<li>2 organic, free range eggs</li>
+		<li>1 tablespoon of organic quinoa</li>
+		<li>1 tablespoon of organic tahini</li>
+		<li>1 tablespoon of corianderseeds</li>
+		<li>1 tablespoon of salt</li>
+		<li>Some lemon juice</li>
+	</ul>
+	<h4>The Sauce</h4>
+	<ul class="ingredients">
+		<li>400 grams of organic tomatoes</li>
+		<li>Mixed spices of your own preference</li>
+		<li>1 Oranic white onion</li>
+	</ul>
+	<h4>The Topping</h4>
+	<ul class="ingredients">
+		<li>125 grams of organic mozarella cheese</li>
+		<li>2 organic carrots</li>
+		<li>75 grams of organic bell pepper</li>
+		<li>3 organic portobello mushrooms</li>
+	</ul>
+	<h3>Procedure</h3>
+	<ol class="procedure">
+		<li>Chop the cabbage, kale, onion and garlic in a mini chopper. Add in the quinoa, tahini, spices and lemon juice. Stir well. Mix the mass with the eggs.</li>
+		<li>Spread the mass in a thin layer on a baking pan.</li>
+		<li>Bake the crust for 20 minutes.</li>
+		<li>Mix the ingredients for the sauce in a hot pan.</li>
+		<li>When the crust is hard and attains a golden glow, take it out and spread out he sauce.</li>
+		<li>Cut the cheese and veggies in thin slices and add it all to the pizza.</li>
+		<li>Bake the pizza for 15 minutes.</li>
+		<li>When the cheese is melting and the pizza looks yummie take it out and dinner is ready to be served!</li>
+	</ol>
+	<table>
+		<caption>Nutritional values</caption>
+		<tr>
+			<th></th>
+			<th>Per slice of pizza</th>
+			<th>Per pizza (10 slices)</th>
+		</tr>
+		<tr>
+			<td>kj/kcal</td>
+			<td>428 kj/ 100 kcal</td>
+			<td>4275 kj / 1011 kcal</td>
+		</tr>
+		<tr>
+			<td>Protein</td>
+			<td>6,8 g</td>
+			<td>67,7 g</td>
+		</tr>
+		<tr>
+			<td>Fat</td>
+			<td>4,7 g</td>
+			<td>47,4 g</td>
+		</tr>
 	<tr>
-		<th></th>
-		<th>Per slice of pizza</th>
-		<th>Per pizza (10 slices)</th>
+		<td>Saturated fats</td>
+		<td>2 g</td>
+		<td>20,5 g</td>
 	</tr>
-	<tr>
-		<td>kj/kcal</td>
-		<td>428 kj/ 100 kcal</td>
-		<td>4275 kj / 1011 kcal</td>
-	</tr>
-	<tr>
-		<td>Protein</td>
-		<td>6,8 g</td>
-		<td>67,7 g</td>
-	</tr>
-	<tr>
-		<td>Fat</td>
-		<td>4,7 g</td>
-		<td>47,4 g</td>
-	</tr>
-<tr>
-	<td>Saturated fats</td>
-	<td>2 g</td>
-	<td>20,5 g</td>
-</tr>
-	<tr>
-		<td>Monounsaturated fats</td>
-		<td>0,6 g</td>
-		<td>5,8 g</td>
-	</tr>
-	<tr>
-		<td>Polyunsaturated fats</td>
-		<td>0,4 g</td>
-		<td>4 g</td>
-	</tr>
-	<tr>
-		<td>Carbohydrates</td>
-		<td>7 g</td>
-		<td>70 g</td>
-	</tr>
-	<tr>
-		<td>Added sugars</td>
-		<td>0,6 g</td>
-		<td>0,06 g</td>
-	</tr>
-	<tr>
-		<td>Dietary fibers</td>
-		<td>2,4 g</td>
-		<td>23,8 g</td>
-	</tr>
-	</table>
-<br/>
-<p>By: Elisabeth Haabegaard</p>
+		<tr>
+			<td>Monounsaturated fats</td>
+			<td>0,6 g</td>
+			<td>5,8 g</td>
+		</tr>
+		<tr>
+			<td>Polyunsaturated fats</td>
+			<td>0,4 g</td>
+			<td>4 g</td>
+		</tr>
+		<tr>
+			<td>Carbohydrates</td>
+			<td>7 g</td>
+			<td>70 g</td>
+		</tr>
+		<tr>
+			<td>Added sugars</td>
+			<td>0,6 g</td>
+			<td>0,06 g</td>
+		</tr>
+		<tr>
+			<td>Dietary fibers</td>
+			<td>2,4 g</td>
+			<td>23,8 g</td>
+		</tr>
+		</table>
+	<br/>
+	<p>By: Elisabeth Haabegaard</p>
+	<hr>
+</div>
+<div class="col2">
+	<h2 class="welcome">Welcome!</h2>
+	<div class="me">
+		<div class="dropdown">
+			<img src="images_elisabeth\27994672_207399000003335_1252007330_n.jpg" alt="Elisabeth in Cali" style="width:115px;height:170px;float:left">My name is Elisabeth and I care about our Earth and all living creatures on the planet. As we are all born as sinners in these times of destruction, we should try to do our best to reduce our footprints on the Earth.
+			<div class="dropdown-content"><img src="images_elisabeth\27994672_207399000003335_1252007330_n.jpg" alt="Elisabeth at Mount San Jacinto, California" width="400px" height="600px">
+		<div class="desc">Elisabeth at Mount San Jacinto, California</div>
+			</div>
+		</div>
+	</div>
+		<h2 class="formularspr">Har du nogle kommentarer eller ønsker, så udfyld venligst nedenstående</h2>
+		<form action="mailto:e.haabegaard@gmail.com" method="post">
+			<p>Hvem henvender sig?</p>
+			<input type="radio" value="Virksomhed" name="afsender" id="afsender" checked tabindex="1"/>Virksomhed&nbsp;&nbsp;&nbsp;
+			<input type="radio" value="Privatperson" name="afsender" id="afsender" checked tabindex="2"/>Privatperson&nbsp;&nbsp;&nbsp;
+			<input type="radio" value="Investor" name="afsender" id="afsender" checked tabindex="3"/>Investor&nbsp;&nbsp;&nbsp;
+			<p>Dit fulde navn</p>
+			<p><input type="text" name="navn" id="navn" tabindex="4"/></p>
+			<p>Din emailadresse</p>
+			<p><input type="text" name="email" id="email" tabindex="5"/></p>
+			<p>Dit telefonnummer</p>
+			<p><input type="text" name="telefonnr" id="telefonnummer" tabindex="6"/></p>
+			<p>Dit køn
+			<select name="køn" id="køn">
+				<option value="Mand" tabindex="7">Mand</option>&nbsp;nbsp;&nbsp;
+				<option value="Kvinde" tabindex="7">Kvinde</option>&nbsp;nbsp;&nbsp;
+				<option value="Ønsker ikke at identificere mig med et køn" tabindex="7">Ønsker ikke at identificere mig med et køn</option>&nbsp;nbsp;&nbsp;
+				<option value="Animalsk afstamning" tabindex="7">Animalsk afstamning</option></select></p>
+				<p>Din alder</p>
+				<p><input type="text" name="alder" id="alder" tabindex="8"/></p>
+				<p>Hvad drejer henvendelsen sig om?</p>
+				<textarea rows="5" name="spørgsmål" id="spørgsmål" cols="30" tabindex="9">Hvad drejer henvendelsen sig om? Uddyb venligst her. </textarea></p>
+				<p><input type="submit" value="Send besked" name="B1" tabindex="10"/>
+				<input type="reset" value="Slet alt" name="B2" tabindex="11"/></p>
+			</form>
+	</div>
 </body>
 </html>
 #
