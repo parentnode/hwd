64b8e9c250076a01f7d66b1cb348b92ddcb8a403
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-14 01:20:02 +0100

    Updating my site, adding CSS

--- src/www/peter.php
+++ src/www/peter.php
@@ -1,25 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>The best site of all time</title>
+	<title>The best website of all time</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
+	<link rel="stylesheet" type="text/css" href="files_peter/HelloGoodLookin.css">
 
-<style>
-body {
-    background-color: linen;
-}
-h1 {
-    color: maroon;
-    margin-left: 40px;
-} 
-</style>
+</head>
 
 <body>
 <h1>Hello there!</h1>
 <h2>Welcome to my site :)</h2>
 
-<div class="ordered-lists-test">
+<div class="ordered-lists-test text">
 	<ol>
 		<li>First</li>
 		<li>Second</li>
@@ -27,7 +19,7 @@ h1 {
 	</ol>
 </div>
 
-<div class="unordered-lists-test">
+<div class="unordered-lists-test text">
 	<ul>
 		<li>Count</li>
 		<li>the rest</li>
@@ -35,20 +27,27 @@ h1 {
 	</ul>
 </div>
 
-<div class="random text">
+<div class="text">
 	<p>A line of text</p>
 	<p>Another line of text</p>
 </div>
 
-<div class="lorem-ipsum text">
+<div class="text">
 	<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum omnis numquam veniam rem atque eos nihil laboriosam ullam dolor repudiandae, quo quae. Voluptate sapiente laborum facere in non perferendis dignissimos!</p>
 </div>
 
-<h3><?php print "php is working!" ?></h3>
+<div class="in-progress">
+	<p><strong>OBS:</strong> This site is currently being worked on, and is not yet finnished.</p>
+</div>
+
+<!-- IGNORE php for now
+<?php print "php is working!" ?>
 <?// phpinfo() ?>
 <?
 	$mysqli = new mysqli("127.0.0.1", "root", "#PASSWORD#");
 	print_r($mysqli);
 ?>
+END COMMENT --> 
+
 </body>
 </html>
