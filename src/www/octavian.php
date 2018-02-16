b6507924ae024ee2bab959fa9a0ba69659d81938
Octavian C Tudora <tudorao@yahoo.com>
2018-02-16 13:56:56 +0100

    html beta
    html

--- src/www/octavian.php
+++ src/www/octavian.php
@@ -6,14 +6,65 @@
 </head>
 <body>
 
-<h1>Hello Octavian</h1>
+<h1>Installing the parentNode web stack on Windows 10</h1>
 <p>
-somthing to write
+
+This guide is currently being updated.
+This will guide you through installing the parentNode web stack on Windows 10
+The parentNode development environment 
+is a full web stack based on Apache,
+MariaDB/MySQL and PHP, which enables you to run all the parentNode tools,
+as well as most PHP based frameworks and CMS', including WordPress, Joomla or TYPO3.
 </p>
 
 
-<p>Hello</p>
+<h2>
+Prerequisites
 
+</h2>
+
+<p>First it has to have an acount on Github.If not then
+make an acount on Github 
+login in Github
+ </p>
+
+<p>
+Download Sourcetree for windows from <ul> <li><a href="https://www.sourcetreeapp.com/">Download Sourcetree</a></li></ul>  
+Install bash shell command line for windows 10 by folowing the guid from the below:
+	<ul> <li><a href="https://www.windowscentral.com/how-install-bash-shell-command-line-Windows-10">Install bash shell command line for windows 10</a></li></ul>
+</p>
+<p>
+Check for  the “Windows Subsystem for Linux” is avalable in the Windows 10 computer by folowing instructions.
+Run CMD in admin mode by cliking right button on CMD icon. 
+type in "bash" in CMD 
+Will be the folowing message if is not instaled in the system.
+"Windows Subsystem for Linux has no installed distributions. 
+Distributions can be installed by visiting the Windows Store: 
+" 
+Put the abouve link in the browser 
+it may complain that does no find the Ubuntu app because the link is old, but it still point to the microsoft app stor.
+Then search for “Ubuntu” app on the search field of that page where thelink was: (“https://aka.ms/wslstoreHYPERLINK "https://aka.ms/wslstore”)"”).
+</p>
+<p>
+"Put the abouve link in the browser 
+it may complain that does no find the Ubuntu app because the link is old, but it still point to the microsoft app stor.
+Then search for “Ubuntu” app on the search field of that page where thelink was mooved so red from below: 
+<ul> <li><a href="https://docs.microsoft.com/en-us/windows/wsl/install-win10">Installing guid </a></li></ul>
+<img src="octavian_files/ubuntu_f_win" >
+<p>
+Run the Ubuntu app 
+It will install a Unbutu distribution 
+Then will be promt for 
+Enter new UNIX username: 
+Enter new UNIX password: 
+</p>
+<p>
+Accessing the windows drive "C: from bash environment or Ubuntu is made with "cd /mnt/c" 
+</p>
+<p>
+ The editor line endings has to be changed to UNIX by for Sublime editor:  "Sublime-> view->line endings-> UNIX" 
+ </p>
+</p>
 </body>
 #
 	Hej
