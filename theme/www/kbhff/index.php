9634040d5923c776d70882a7b52bfb85f0220f8a
Martin Kæstel Nielsen <martin@think.dk>
2018-02-22 15:01:02 +0100

    guide

--- theme/www/kbhff/index.php
+++ theme/www/kbhff/index.php
@@ -10,6 +10,21 @@ Introduction
 
 
 Table of content
+<ul>
+	<li><a href="software-license">Software license summary</a></li>
+	<li><a href="choose-an-editor">Choosing an Editor</a></li>
+</ul>
+
+
+
+Setting up the development environment
+Install the webstack / Working with Git
+
+Set up playground (practice area) (class-test)
+
+Before you start coding (Code conventions)
+
+
 
 
 </body>
