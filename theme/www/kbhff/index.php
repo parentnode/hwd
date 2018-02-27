af77dfe6aff4350f7c5ce8020000a97458bfc53e
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-27 13:46:57 +0100

    semantics

--- theme/www/kbhff/index.php
+++ theme/www/kbhff/index.php
@@ -6,17 +6,24 @@
 </head>
 <body>
 
-<h1>Introduction</h1>
+<div id="page">
+	<div id="header">
+	<div id="content">
+		<h1>Introduction</h1>
+		<h2>Table of content</h2>
+		<ul>
+			<li><a href="software-license">Software license summary</a></li>
+			<li><a href="choose-editor">Choosing an Editor</a></li>
+			<li><a href="development-environment">Setting up the development environment (Install the webstack) (Working with Git)</a></li>
+			<li><a href="before-you-start-coding">Before you start coding (Code conventions)</a></li>
+			<li><a href="playground">Set up playground (practice area) (class-test)</a></li>
+		</ul>
+	</div>
+	<div id="navigation">
+	<div id="footer">
+</div>
 
 
-<h2>Table of content</h2>
-<ul>
-	<li><a href="software-license">Software license summary</a></li>
-	<li><a href="choose-editor">Choosing an Editor</a></li>
-	<li><a href="development-environment">Setting up the development environment (Install the webstack) (Working with Git)</a></li>
-	<li><a href="before-you-start-coding">Before you start coding (Code conventions)</a></li>
-	<li><a href="playground">Set up playground (practice area) (class-test)</a></li>
-</ul>
 
 
 
