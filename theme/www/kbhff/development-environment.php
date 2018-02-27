f08cd2c4b1041f9d1acb0d88f794283ceb809422
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-02-27 11:55:35 +0100

    fixing spaces, quotes and readabillity

--- theme/www/kbhff/development-environment.php
+++ theme/www/kbhff/development-environment.php
@@ -9,12 +9,18 @@
 <h1>Setting up the development environment</h1>
 
 <div>
-    <h2>Installing the webstack</h2>
-    <p>So what is a webstack? Well it's a collection of software you'll need to have before you can start building anything on the web. It's the foundation, we build on.</p>
-    <p>The most basic forms of webstack are usually comprised of an Operating systen, a Http-server, an Application-server and a Database server.</p>
-    <p>An example could be the "LAMP" stack which runs on a Linux operating system and uses the Apache Http-server, MariaDB/MySQL database and PHP application server.</p>
-    <p>Since the project is based on the parentNode webstack, you'll first need to install it.</p>
-    <ul>
+	<h2>Installing the webstack</h2>
+	<p>
+	So what is a webstack? Well it's a collection of software you'll need to have before you can start building anything on the web. 
+	It's the foundation, we build on.
+	</p>
+	<p>The most basic forms of webstack are usually comprised of an Operating systen, a Http-server, an Application-server and a Database server.</p>
+	<p>
+	An example could be the <em>LAMP</em> stack which runs on a Linux operating system and uses the Apache Http-server, 
+	MariaDB/MySQL database and PHP application server.
+	</p>
+	<p>Since the project is based on the parentNode webstack, you'll first need to install it.</p>
+	<ul>
 		<li><a href="https://parentnode.dk/blog/the-parentnode-web-stack">You can read more about the parentNode webstack here.</a></li>
 		<li><a href="https://parentnode.dk/blog/installing-web-stack-on-windows-10">Follow this guide to get started on Windows.</a></li>
 		<li><a href="https://parentnode.dk/blog/installing-the-web-stack-on-mac-os">Follow this one for Mac.</a></li>
@@ -22,19 +28,25 @@
 </div>
 
 <div>
-    <h2>Working with git</h2>
-    <p>Git is a "version control system" or "VCS" for short, it essentially allows multiple people to work on the same project.</p>
-    <p>As Git is just a program we will need a "Host" where we can upload our projects, or contribute to existing ones.</p>
-    <p>The hosting platform we will use is "GitHub", since it's the most popular host around and is completely free! This is also where kbhff is hosted.</p>
-    <p><a href="https://guides.github.com/activities/hello-world/">Here's how to get started with GitHub</a></p>
-    <p>Working with GitHub through the browser isn't always what you want so you should download a GitHub client</p>
-    <p>We recommend GitHub desktop starting out as it's free and the simplest way to use Git.</p>
-	<p>Github Desktop is a bit different from the website but there's alot of upsides to using a client, such as easily being able to update and upload your files, there are other clients with more features at the expense of usability.</p>
-    <ul>
-        <li><a href="https://desktop.github.com/">Here's a link to GitHub dekstop.</a></li>
-        <li><a href="https://help.github.com/desktop/guides/getting-started-with-github-desktop/">If you need help to install and set up the program.</li>
-        <li><a href="https://help.github.com/desktop/guides/contributing-to-projects/">Here's how to use the client.</li>
-    </ul>
+	<h2>Working with git</h2>
+	<p>Git is a <em>version control system</em>, it essentially allows multiple people to work on the same project.</p>
+	<p>As Git is just a program we will need a host where we can upload our projects, or contribute to existing ones.</p>
+	<p>
+	The hosting platform we will use is <strong>GitHub</strong>, since it's the most popular host around and is completely free! 
+	This is also where kbhff is hosted.
+	</p>
+	<p><a href="https://guides.github.com/activities/hello-world/">Here's how to get started with GitHub</a></p>
+	<p>Working with GitHub through the browser isn't always what you want so you should download a GitHub client</p>
+	<p>We recommend GitHub desktop starting out as it's free and the simplest way to use Git.</p>
+	<p>
+	Github Desktop is a bit different from the website but there's alot of upsides to using a client, such as easily being able to update and upload your files,
+	there are other clients with more features at the expense of usability.
+	</p>
+	<ul>
+		<li><a href="https://desktop.github.com/">Here's a link to GitHub dekstop.</a></li>
+		<li><a href="https://help.github.com/desktop/guides/getting-started-with-github-desktop/">If you need help to install and set up the program.</li>
+		<li><a href="https://help.github.com/desktop/guides/contributing-to-projects/">Here's how to use the client.</li>
+	</ul>
 </div>
 
 </body>
