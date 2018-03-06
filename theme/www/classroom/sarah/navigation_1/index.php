d739dd2ea5a2d69d494d0e3b593b2ceed2621b96
Sarah E <>
2018-03-06 15:22:05 +0100

    css for politico navigation

--- theme/www/classroom/sarah/navigation_1/index.php
+++ theme/www/classroom/sarah/navigation_1/index.php
@@ -3,8 +3,60 @@
 <head>
 	<title>Page title</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<style type="text/css">
+
+/* Style navigation to look like https://www.politico.com/ */
+
+		#navigation ul {
+			list-style: none;
+			background: #840000;
+			font-family: Arial,sans-serif;
+			font-size: 16px;
+			padding: 1.6875em;
+			margin: 0;
+		}
+/* Need to add min width to window of 42.875em */
+
+		#navigation a {
+			color: #fff;
+			text-decoration: none;
+		}
+
+		#navigation li {
+			border-bottom-width: 2px;
+			border-bottom-style: solid;
+			border-bottom-color: #6b0000;
+/* Same as border-bottom: 2px solid #6b0000;*/
+			padding-bottom: 0.4em;
+/* Same as padding: 0 0 0.4em 0; */
+			margin-bottom: 0.4em;
+			margin-left: 1.412429378531%;
+			line-height: 1em;
+			font-weight: bold;
+			width: 15.489642184557%;
+			display: inline-block;
+		}
+
+		#navigation li:first-child {
+			margin-left: 0;
+		}
+
+		/* .vertical-group { */
+			/* font-size: 24px; */
+			/* width: 15.489642184557%; */
+		/* } */
+
+		/* .vertical-group:first-child { */
+			/* margin-left: 0; */
+		}
+/* First child of navigation has to have margin-left of 0: .vertical-group:first-child */
+
+		/* For each vertical element: */
+		/* width:15.489642184557%; */
+		/* margin-left:1.412429378531%; */
+		/* float:left; */
+		/* border: none */
 
 	</style>
 </head>
@@ -15,14 +67,18 @@
 	<div id="content"></div>
 	<div id="navigation">
 		<ul>
-			<li><a href="software-license">Software license summary</a></li>
+			<!-- <div class="vertical-group"> -->
+			<li><a href="software-license">Software license</a></li>
+			<!-- </div> -->
+			<!-- <div class="vertical-group"> -->
 			<li><a href="choose-editor">Choosing an Editor</a></li>
-			<li><a href="development-environment">Setting up the development environment</a></li>
+			<!-- </div> -->
+			<li><a href="development-environment">Setting up environment</a></li>
 			<li><a href="before-you-start-coding">Before you start coding</a></li>
 			<li><a href="playground">Set up playground</a></li>
 			<li><a href="dictionary">Dictionary</a></li>
 		</ul>
-		
+
 	</div>
 	<div id="footer"></div>
 </div>
