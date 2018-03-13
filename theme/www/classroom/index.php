1ab9a5ad4ccda37e00dbdfcc88f4b2fa9c1811f6
Martin Kæstel Nielsen <martin@think.dk>
2018-03-13 16:03:00 +0100

    Updated structure

---
+++ theme/www/classroom/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Class Test Area</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+<h1>Class Room</h1>
+
+<ul>
+	<li><a href="dorte/dorte.php">Dorte</a></li>
+	<li><a href="soren/index.php">Søren</a></li>
+	<li><a href="mads/mads.php">Mads</a></li>
+	<li><a href="elisabeth/elisabeth.php">Elisabeth</a></li>
+	<li><a href="sarah/sarah.php">Sarah</a></li>
+	<li><a href="peter/peter.php">Peter</a></li>
+	<li><a href="octavian/octavian.php">Octavian</a></li>
+</ul>
+
+</body>
+</html>
--- theme/www/index.php
+++ theme/www/index.php
@@ -9,15 +9,7 @@
 <h1>Hello World</h1>
 
 <ul>
-	<li><a href="dorte.php">Dorte</a></li>
-	<li><a href="bolette.php">Bolette</a></li>
-	<li><a href="mads.php">Mads</a></li>
-	<li><a href="elisabeth.php">Elisabeth</a></li>
-	<li><a href="sarah.php">Sarah</a></li>
-	<li><a href="peter.php">Peter</a></li>
-	<li><a href="octavian.php">Octavian</a></li>
-
-	<li><a href="new-participant.php">New participant</a></li>
+	<li><a href="/classroom">Class room</a></li>
 
 	<li><a href="/kbhff">KBHFF guide</a></li>
 </ul>
