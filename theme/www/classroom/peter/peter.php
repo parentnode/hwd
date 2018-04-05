6b4f3dfa0952cd64232f4b1f597411725f40594e
Threepwoodd <36038081+Threepwoodd@users.noreply.github.com>
2018-04-05 10:56:56 +0200

    Making KBHFF draf available

---
+++ theme/www/classroom/peter/peter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Peter's portal</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+<h1>Portal of awesome</h1>
+
+<ul>
+	<li><a href="/classroom/peter/files_peter/idontevenknow/peter.php">I have no clue what this is</a></li>
+	<li><a href="/classroom/peter/files_peter/PSS/main/startpage.html">PeterStreamingService</a></li>
+	<li><a href="/classroom/peter/files_peter/KBHFF_draft/login.html">KBHFF Login</a></li>
+</ul>
+
+</body>
+</html>
