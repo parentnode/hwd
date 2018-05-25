b9d39ed44174c7b6228ebc2c60e472f474272bf5
tudorao <tudorao@yahoo.com>
2018-05-25 11:22:44 +0200

    316

--- theme/www/homework/octavian/316/js/i-page.js
+++ theme/www/homework/octavian/316/js/i-page.js
@@ -3,15 +3,21 @@ Util.Objects["page"] = new function() {
 		u.bug("init page");
 
 
-		// global scroll handler 
+	
+
 		page.resized = function() {
 			// u.bug("page resized");
+			var scene = u.qs(".scene");
+			u.ass(scene, {"height":"u.browserH()"});
+			console.log(u.browserH());
 
 		}
 
 		// global scroll handler 
 		page.scrolled = function() {
 			// u.bug("page scrolled");
+
+			console.log('scrolled');
 
 		}
 
@@ -22,27 +28,31 @@ Util.Objects["page"] = new function() {
 			// page is ready to be shown - only initalize if not already shown
 			if(!this.is_ready) {
 
-				// page is ready
+				// page is ready?
 				this.is_ready = true;
 
 				// Get current scene
 				this.scene = u.qs(".scene", this);
 
 
-				// set resize handler
+				// resize handler
 				u.e.addEvent(window, "resize", this.resized);
-				// set scroll handler
+				// scroll handler
 				u.e.addEvent(window, "scroll", this.scrolled);
 
 
 				// initial resize
 				this.resized();
 			}
+			u.a.transition(this.scene, "opacity 1s ease-in");
+			u.ass(this.scene, {
+				"opacity":"1"
+			})
 		}
 
 		page.ready();
 	}
 }
 
-// Start initialization on DOMReady
+// initialization on DOMReady
 u.e.addDOMReadyEvent(u.init)
