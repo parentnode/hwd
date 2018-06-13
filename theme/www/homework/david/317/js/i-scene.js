9215264d3610bb870571b2b6cd637546846fcae1
jessen1989 <jessen-it@outlook.com>
2018-06-13 18:05:03 +0200

    added 317

--- theme/www/homework/david/317/js/i-scene.js
+++ theme/www/homework/david/317/js/i-scene.js
@@ -3,26 +3,29 @@ Util.Objects["scene"] = new function() {
 
 		console.log("Scene init");
 		console.log(scene);
-		this.form = u.qs(".form");
-		u.f.init(this.form);
-		var i;
-		var j;
-		this.test = u.qs(".field .string");
-		this.testall = u.qsa("div",this.test);
-		this.form.submitted = function(){
-			console.log("Submitted");
-			for(j= 0; j < this.testall.length; j++)
-			{
-				u.a.transition(this.testall[j],"all 5s ease-out");
-				u.as(this.testall[j],"opacity","0");
-
+		scene.form = u.qs("form");
+		u.f.init(scene.form);
+		scene.show = function (){
+			for(var i= 0; i < scene.testall.length; i++){
+				console.log(scene.testall[i]);
+				u.a.transition(scene.testall[i],"all 5s ease-out");
+				u.as(scene.testall[i],"opacity","1");
 			}
 		}
-		
-		for(i= 0; i < this.testall.length; i++){
-			console.log(this.testall[i]);
-			u.a.transition(this.testall[i],"all 5s ease-out");
-			u.as(this.testall[i],"opacity","1");
+		scene.hide = function (){
+			for(var i= 0; i < scene.testall.length; i++){
+				console.log(scene.testall[i]);
+				u.a.transition(scene.testall[i],"all 5s ease-out");
+				u.as(scene.testall[i],"opacity","0");
+			}
 		}
+		scene.test = u.qs(".field .string");
+		scene.testall = u.qsa("div",scene.test);
+		scene.show();
+		scene.form.submitted = function(){
+			console.log("Submitted");
+			scene.hide();
+		}	
+		
 	}
 }
