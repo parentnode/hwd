e363a1573047718a4319d5e825da30042f97f662
munkefrugt <munkefrugt@gmail.com>
2021-12-08 13:25:11 +0100

    d

--- theme/www/homework/martin/503/index.php
+++ theme/www/homework/martin/503/index.php
@@ -1,57 +1,102 @@
 <?php
+	
+$indexed[] = "I want out";
+$indexed[] = "You want out";
+$indexed[] = "They want out";
+$indexed[] = "We all want out";
+array_push($indexed, "Don't print me");
+$indexed[] = "Into the sunshine";
 
+$associative = ["First" => "I want out"];
+$associative["Second"] = "You want out";
+$associative["Third"] = "They want out";
+$associative["Forth"] = "We all want out";
+$associative["Sixth"] = "Don't print me";
+$associative["fifth"] = "Into the sunshine";
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+$multidimensional["clients"]["Need"] = "The sunshine";
+$multidimensional["babies"] = ["Need" => "The milk"];
+$multidimensional["clients"]["Have"] = "The money";
+$multidimensional["clients"]["Want"] = "The sugar";
+$multidimensional["clients"]["Argh"] = "Don't print me";
+$multidimensional["babies"]["Have"] = "The cuteness";
+$multidimensional["babies"]["Want"] = "The boob";
+$multidimensional["aliens"] = ["Want" => "The blood", "Have" => "The tech", "Need" => "The love", "Facts" => ["0", "0", "0"]];
+	
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
-
-
-
-$mail = new PHPMailer(true);
-
-
-
-try {
-	//Server settings
-	$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-	$mail->isSMTP();                                            //Send using SMTP
-	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	$mail->Username   = 'munkefrugt@gmail.com';                     //SMTP username
-	$mail->Password   = 'secret';                               //SMTP password
-	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-	$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-
-	//Recipients
-	//$mail->setFrom('from@example.com', 'Mailer');
-	$mail->addAddress('munkefrugt@gmail.com', 'Joe User');     //Add a recipient
-	//$mail->addAddress('ellen@example.com');               //Name is optional
-	//$mail->addReplyTo('info@example.com', 'Information');
-	//$mail->addCC('cc@example.com');
-	//$mail->addBCC('bcc@example.com');
-
-	//Attachments
-	//$mail->addAttachment('test.txt');         //Add attachments
-	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-	//Content
-	$mail->isHTML(true);                                  //Set email format to HTML
-	$mail->Subject = 'Here is the subject';
-	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-	$mail->send();
-	echo 'Message has been sent';
-} catch (Exception $e) {
-	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Exercise 502</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+
+
+
+<div id="page" class="i:page">
+	<div id="header"></div>
+	<div id="content">
+
+		<div class="scene">
+
+			<!-- lav 2 versioner php med html eller html med php i -->
+			<!-- 3 typer strege, ' " eller ren html -->
+			<!-- hints -->
+			<!-- https://stackoverflow.com/questions/1813098/php-array-to-list/17501916 -->
+			<!-- what is the double arrow?  -->
+			<!-- https://stackoverflow.com/questions/14037290/what-does-this-mean-in-php-or -->
+			
+			 <ul>
+			<?php
+			// for each associative
+			for ($i=0; $i < count($indexed); $i++) { 
+				if($indexed[$i] != "Don't print me"){
+					echo "<li>$indexed[$i]</li>";
+				}
+			}
+			
+			?>
+			</ul>
+
+			<dt>
+			<?php
+			// for each associative
+			foreach($associative as $key => $value) {
+				if($value != "Don't print me"){
+					echo "<dt>$key</dt>";
+					echo "<dd>$value</dd>";
+					echo "<br>";	
+				}	
+			}
+			?>
+			</dt>
+
+			
+		
+			<?php 
+			// multidimensional
+			foreach( $multidimensional as $outer_key => $outer_val) {
+				if($outer_key != "aliens"){
+					echo "<h2>".$outer_key."</h2><br>";
+
+					foreach($outer_val as $innner_val){
+						if($innner_val != "Don't print me"){
+							echo "$innner_val . <br>";
+						}
+					}
+				}
+			} 
+			?>
+
+		</div>
+
+	</div>
+	<div id="footer"></div>
+</div>
+
+</body>
+</html>
